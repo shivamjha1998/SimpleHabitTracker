e09a77c199b80aa3e82248346440b7b0e18fb576
Shivam Jha <shivamjha98@gmail.com>
2024-11-10 19:39:16 +0000

    Add common style-sheet to the project.

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -3,12 +3,13 @@ import React, {useContext} from 'react';
 import {View, Text, Button} from 'react-native';
 import {HabitContext} from '../context/HabitContext';
 import DailyCheckbox from '../components/DailyCheckbox';
+import styles from '../styles/styles';
 
 const HomeScreen = ({navigation}) => {
   const {habitData, toggleDay} = useContext(HabitContext);
 
   return (
-    <View>
+    <View style={styles.backgroundColor}>
       <Text>Daily Habit Tracker</Text>
       {habitData.map((isChecked, index) => (
         <DailyCheckbox
---
+++ styles/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    backgroundColor: {
+        backgroundColor: '#FEFAE0'
+    }
+})
+
+export default styles;
